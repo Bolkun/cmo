b6309199a6a8cc2ba5906ca51ccdf012422caedd
Bolkun <serhiy.bolkun1@tu-dresden.de>
2023-10-29 15:11:32 +0100

    Firebase files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyDk7wZEy-WDZqiIER8TqLV2cizjR2opaLA',
     authDomain: 'cmo-game.firebaseapp.com',
